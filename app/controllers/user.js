3258b6d91038337beb88441a13ec559125665950
Florian Knop <florian_knop@hotmail.com>
2015-11-02 17:10:31 +0100

    Add missing user routes

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -34,3 +34,51 @@ exports.put = {
         reply('');
     }
 };
+exports.getTags = {
+    description: 'Get the user\'s tag',
+    handler: function(request, reply) {
+        reply('');
+    }
+};
+
+exports.getCourses = {
+    description: 'Get the courses (subscribed)',
+    handler: function(request, reply) {
+        reply('');
+    }
+};
+
+exports.subscribeToCourse = {
+    description: 'Subscribe to a course',
+    handler: function(request, reply) {
+        reply('');
+    }
+};
+
+exports.unsubscribeToCourse = {
+    description: 'Unsubscribe to a course',
+    handler: function(request, reply) {
+        reply('');
+    }
+};
+
+exports.addFolder = {
+    description: 'Add a folder',
+    handler: function(request, reply) {
+        reply('');
+    }
+};
+
+exports.addCourseToFolder = {
+    description: 'Add a course to the folder (removes from the old folder)',
+    handler: function(request, reply) {
+        reply('');
+    }
+};
+
+exports.put = {
+    description: 'Update user',
+    handler: function(request, reply) {
+        reply('');
+    }
+};
--- app/routes/api.js
+++ app/routes/api.js
@@ -15,14 +15,27 @@ exports.register = function (server, options, next) {
     server.route([
         {method: 'GET', path: '/users', config: Controllers.User.getAll},
         {method: 'GET', path: '/users/{id}', config: Controllers.User.get},
+        {method: 'GET', path: '/users/{id}/tags', config: Controllers.User.getTags},
+        {method: 'GET', path: '/users/{id}/courses', config: Controllers.User.getCourses},
         {method: 'POST', path: '/users', config: Controllers.User.post},
+        {method: 'POST', path: '/users/{id}/courses/{courseId}/subscribe',
+            config: Controllers.User.subscribeToCourse},
+
+        {method: 'POST', path: '/users/{id}/courses/{courseId}/unsubscribe',
+            config: Controllers.User.unsubscribeToCourse},
+
+        {method: 'POST', path: '/users/{id}/folders/{name}', config: Controllers.User.addFolder},
+        {method: 'POST', path: '/users/{id}/folders/{name}/courses/{courseId}',
+            config: Controllers.User.addCourseToFolder},
+
         {method: 'PUT', path: '/users/{id}', config: Controllers.User.put},
         {method: 'DELETE', path: '/users/{id}', config: Controllers.User.delete},
-		{ method: 'GET', path: '/tags', config:  Controllers.Tag.getAll},
-		{ method: 'GET', path: '/tags/{id}', config: Controllers.Tag.get},
-		{ method: 'POST', path: '/tags', config: Controllers.Tag.post},
-		{ method: 'DELETE', path: '/tags/{id}', config: Controllers.Tag.delete}
-	]);
+
+        {method: 'GET', path: '/tags', config:  Controllers.Tag.getAll},
+        {method: 'GET', path: '/tags/{id}', config: Controllers.Tag.get},
+        {method: 'POST', path: '/tags', config: Controllers.Tag.post},
+        {method: 'DELETE', path: '/tags/{id}', config: Controllers.Tag.delete}
+    ]);
 
     next();
 };
