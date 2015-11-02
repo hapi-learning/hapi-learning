b63b5eb04f221dbe24c55fbf8d18a68c934196f6
splacentino <39631@heb.be>
2015-11-02 15:14:18 +0100

    Add folder schema

---
+++ app/models/folder.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function (sequelize, DataTypes) {
+	return sequelize.define('Folder', {
+		name: {
+			type: DataTypes.STRING,
+			unique: false,
+			allowNull: false,
+			field: 'name'
+		},
+		description: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'description'
+		}
+	}, {
+		paranoid: true,
+		tableName: 'folders',
+		underscored: true
+	});
+}
