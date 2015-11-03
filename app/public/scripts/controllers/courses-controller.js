937f0da8164acb10833f2ddf8985bed5ab11f36b
U-Asus\Gatien <gatien.bovyn@gmail.com>
2015-11-03 13:36:17 +0100

    Add teachers info to course (WIP)

--- app/public/scripts/controllers/courses-controller.js
+++ app/public/scripts/controllers/courses-controller.js
@@ -11,17 +11,47 @@ angular.module('hapi-learning')
     }]);
 
 
+angular.module('hapi-learning')
+    .directive('teacherInfo', function() {
+   return {
+       restrict: 'EAC',
+       template: '<a ng-href="mailto: {{teacher.acronyme | lowercase}}@heb.be">{{teacher.name}} ({{teacher.acronyme}})</a>'
+//       templateUrl: '../directives/teacher-info.html'
+   };
+});
+
+
 var courses = [
     {
         name: 'Atelier logiciel 3e',
         code: 'ATL',
         description: 'Labo consistant à nous faire aimer le Java. A échoué jusqu’à présent.',
         tags: ['3e', 'labo'],
+        teachers: [
+            {
+                name: 'F. Pluquet',
+                acronyme: 'FPL'
+            }
+            ,
+            {
+                name: 'F. Servais',
+                acronyme: 'SRV'
+            }],
     },
     {
         name: 'Algorithmique 2e',
         code: 'ALG',
         description: 'Cours théorique de logique basique.',
         tags: ['2e', 'théorique'],
+        teachers: [
+        {
+                name: 'N. Pettiaux',
+                acronyme: 'NPX'
+            }
+            ,
+            {
+                name: 'L. Beeckmans',
+                acronyme: 'LBC'
+            }],
      }
 ];
