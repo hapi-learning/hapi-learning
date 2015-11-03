8ac2f5068f3ceaed3cff0936768bf4b05ff78e43
U-Asus\Gatien <gatien.bovyn@gmail.com>
2015-11-03 13:10:33 +0100

    Add subscribe/unsubscribe button

--- app/public/scripts/controllers/courses-controller.js
+++ app/public/scripts/controllers/courses-controller.js
@@ -1,24 +1,27 @@
 angular.module('hapi-learning')
     .controller('courses-controller', ['$scope', function ($scope) {
 
-        
-
         $scope.courses = courses;
-        $scope.cpt = 2;
+        
+       $scope.subscribed = function() {
+           // did the user already subscribe to the course?
+           return false;
+       };
+
     }]);
 
 
 var courses = [
-            {
-                name: 'Atelier logiciel 3e',
-                code: 'ATL',
-                description: 'Labo consistant à nous faire aimer le Java. A échoué jusqu’à présent.',
-                tags: ['3e', 'labo']
-                },
-            {
-                name: 'Algorithmique 2e',
-                code: 'ALG',
-                description: 'Cours théorique de logique basique.',
-                tags: ['2e', 'théorique']
-                }
-            ];
+    {
+        name: 'Atelier logiciel 3e',
+        code: 'ATL',
+        description: 'Labo consistant à nous faire aimer le Java. A échoué jusqu’à présent.',
+        tags: ['3e', 'labo'],
+    },
+    {
+        name: 'Algorithmique 2e',
+        code: 'ALG',
+        description: 'Cours théorique de logique basique.',
+        tags: ['2e', 'théorique'],
+     }
+];
