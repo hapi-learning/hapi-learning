203491e5c38d6c2f284d410d52cdaef5ab7f2a94
Florian Knop <florian_knop@hotmail.com>
2015-11-04 00:49:07 +0100

    Remove password from select

--- app/controllers/course.js
+++ app/controllers/course.js
@@ -113,59 +113,72 @@ exports.post = {
         }
     },
     handler: function (request, reply) {
+
         const Course = this.models.Course;
         const User = this.models.User;
         const Tag = this.models.Tag;
 
         const hasTitulars = request.payload.titulars ? true : false;
         const hasTags = request.payload.tags ? true : false;
+        const coursePayload = {
+            name: request.payload.name,
+            code: request.payload.code,
+            description: request.payload.description
+        };
 
-        const setTags = hasTags ? Promise.resolve(Tag.findAll({
-            where: {
-                name: {
-                    $in: request.payload.tags
-                }
-            }
-        })) : Promise.resolve([]);
+        // If tags has been passed to the payload, return a promise
+        // loading the tags, otherwise return a promise returning an empty array
+        const getTags = hasTags
+            ? Promise.resolve(Tag.findAll({where: {name: {$in: request.payload.tags}}}))
+            : Promise.resolve([]);
 
-        const setUsers = hasTitulars ? Promise.resolve(User.findAll({
-            where: {
-                username: {
-                    $in: request.payload.titulars
-                }
-            }
-        })) : Promise.resolve([]);
+        // If titulars has been passed to the payload, return a promise
+        // loading the titulars, otherwise return a promise returning an empty array
+        const getTitulars = hasTitulars
+            ? Promise.resolve(User.findAll(
+                {where: {username: {$in: request.payload.titulars}},
+                 attributes: {exclude: ['password']}}))
+            : Promise.resolve([]);
 
-        // TODO -- FIXME
-        // -> Handle create (same unique key)
-        // -> Return titulars and tags array
+        // Loads tags and titulars to be added
+        Promise
+        .all([getTags, getTitulars])
+        .then(values => {
 
-        Promise.all([setTags, setUsers])
-            .then(values => {
                 const tags = values[0];
                 const titulars = values[1];
 
-                const a = (hasTitulars && titulars.length !== request.payload.titulars.length);
-                const b = (hasTags && tags.length !== request.payload.tags.length);
+                const wrongTitulars = (hasTitulars && titulars.length !== request.payload.titulars.length);
+                const wrongTags = (hasTags && tags.length !== request.payload.tags.length);
 
-                if (a || b) {
-                    return reply(Boom.badData(a ? 'Invalid titular(s)' : 'Invalid tag(s)'));
-                } else {
+                if (wrongTitulars || wrongTags)
+                {
+                    return reply(Boom.badData(wrongTitulars ? 'Invalid titular(s)' : 'Invalid tag(s)'));
+                }
+                else
+                {
+                    // TODO - Should refactor this somewhere else.
+                    // Create course
+                    Course
+                    .create(coursePayload)
+                    .then(newCourse => {
 
-                    Course.create({
-                            name: request.payload.name,
-                            code: request.payload.code,
-                            description: request.payload.description
+                        // Add titulars and tags to the new added course
+                        Promise
+                        .all([newCourse.addTitulars(titulars), newCourse.addTags(tags)])
+                        .then(() => {
+
+                            // Build response
+                            let course = newCourse.get({plain:true});
+                            course.tags = [];
+                            course.titulars = [];
+                            tags.forEach(t => course.tags.push(t));
+                            titulars.forEach(t => course.titulars.push(t));
+
+                            return reply(JSON.stringify(course, null, 4));
                         })
-                        .then(course => {
-                            Promise.all([course.addTitulars(titulars),
-                                         course.addTags(tags)])
-                                .then(() => {
-                                    course.tags = tags;
-                                    course.titulars = titulars;
-                                    return reply(course);
-                                });
-                        });
+                        .catch(() => {/*TODO*/});
+                    });
                 }
             });
     }
