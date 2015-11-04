269f52348ae39c9f758a90542d28afb7e110effa
U-Asus\Gatien <gatien.bovyn@gmail.com>
2015-11-04 14:28:30 +0100

    Fix multiplicities

--- app/models/index.js
+++ app/models/index.js
@@ -35,26 +35,26 @@ exports.register = function(server, options, next) {
 
         // A Course has multiple Tags to describe him
         m.Course.belongsToMany(m.Tag, { through: 'course_tags' });
-
-        // An User can subscribe to many Courses (not in a Folder)
-        m.Course.belongsToMany(m.User, { as: 'Users', through: 'user_courses' });
-
+        
+        // A Course can have multiple Users as Titulars
         m.Course.belongsToMany(m.User, { as: 'Titulars',  through: 'course_titulars'});
 
+        // An User can subscribe to many Courses (not in a Folder)
+        m.User.belongsToMany(m.Course, { as: 'Courses', through: 'user_courses' });
+
         // An User can create many Folders containing Courses
-        m.Folder.belongsToMany(m.User, { through: 'user_folders'});
+        m.User.belongsToMany(m.Folder, { through: 'user_folders'});
 
         // A Folder contains many Courses
-        m.Course.belongsToMany(m.Folder, { through: 'user_courses_folders'});
+        m.Folder.belongsToMany(m.Course, { through: 'user_courses_folders'});
 
         // An User can have multiple Tags (for example 'A12' + 'gestion')
         m.User.belongsToMany(m.Tag, { through: 'user_tags' });
 
-         // A permission can belongs to many roles
-        m.Permission.belongsToMany(m.Role, { through: 'role_permissions' });
+        // A Role can have multiple Permissions
+        m.Role.belongsToMany(m.Permission, { through: 'role_permissions' });
 
-
-        // A user has a role
+        // A User has a Role
         m.User.belongsTo(m.Role);
 
         // A user can have specifics additional permissions.
