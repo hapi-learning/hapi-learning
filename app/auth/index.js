1fb5efe393324fb895eb19f7b050dd5e2085c73e
Florian Knop <florian_knop@hotmail.com>
2015-11-12 13:44:17 +0100

    add validation function (auth)

--- app/auth/index.js
+++ app/auth/index.js
@@ -1,12 +1,39 @@
 'use strict';
 
 
+/*
+const payload = {
+    id: result.id,
+    username: result.username,
+    email: result.email,
+    role: result.Role.name,
+    isAdmin: (result.Role.name === 'admin')
+};
+
+*/
+
+
 exports.register = function (server, options, next) {
 
     server.auth.strategy('jwt', 'jwt', {
         key: process.env.AUTH_KEY || 'dJa1O65Yb25MNjq451NxvZb4tAxWQla1',
         validateFunc: function(decoded, request, callback) {
-            return callback(null, true, {scope: decoded.role}); // verification ok(err, isValid, credential);
+
+            const User = server.plugins.models.models.User;
+
+            User.findOne({
+                where: {
+                    id: decoded.id,
+                    username: decoded.username,
+                    email: decoded.email,
+                }
+            }).then(result => {
+               if (result && result.Role.name === decoded.role) {
+                   return callback(null, true, {scope: decoded.role});
+               } else {
+                   return callback(null, false);
+               }
+            });
         },
         verifyOptions: {
             algorithms: [ 'HS256' ]
