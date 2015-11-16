cd7ef45b0840ed6c3a1db1eee67c16342a6a8f42
Florian Knop <florian_knop@hotmail.com>
2015-11-16 21:53:05 +0100

    Fix expired token on angular app

--- app/server.js
+++ app/server.js
@@ -138,7 +138,7 @@ Glue.compose(internals.manifest, {relativeTo: __dirname}, (err, server) => {
     var Models = server.plugins.models.models;
 
     Models.sequelize.sync({
-        force: true // drops all db and recreates them
+        force: false // drops all db and recreates them
        // logging: console.log
     })
     .then(() => {
