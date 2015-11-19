90fa703721beabd7e9a9abfaff3c33d5e695edad
Florian Knop <florian_knop@hotmail.com>
2015-11-19 23:26:08 +0100

    Add redirection when course url is invalid

--- app/public/scripts/controllers/course-controller.js
+++ app/public/scripts/controllers/course-controller.js
@@ -1,25 +1,32 @@
 angular.module('hapi-learning')
     .controller('CourseCtrl', ['$scope', '$stateParams',
                                'CoursesFactory', 'LoginFactory',
+                               '$state',
                 function ($scope, $stateParams,
-                          CoursesFactory, LoginFactory) {
+                          CoursesFactory, LoginFactory, $state) {
 
-            $scope.course = {};
 
-            CoursesFactory.loadSpecific($stateParams.code)
-            .then(function (course) {
-                if (course)
-                {
-                    $scope.course.name = course.name;
-                    $scope.course.description = course.description;
-                    $scope.course.code = course.code;
-                    $scope.course.teachers = course.teachers;
-                    $scope.course.tags = course.tags;
-                }
-                else
-                {
-                    console.log('Course not found');
-                }
-            })
-            .catch(function (error) {console.log(error);});
+            $scope.updateCourse = false;
+
+            if (!$scope.course && !$scope.updateCourse) {
+                CoursesFactory.loadSpecific($stateParams.code)
+                .then(function (course) {
+                    if (course)
+                    {
+                        $scope.course = {};
+                        $scope.course.name = course.name;
+                        $scope.course.description = course.description;
+                        $scope.course.code = course.code;
+                        $scope.course.teachers = course.teachers;
+                        $scope.course.tags = course.tags;
+                    }
+                    else
+                    {
+                        $state.go('root.home');
+                    }
+                })
+                .catch(function (error) {
+                    $state.go('root.home');
+                });
+            }
     }]);
