734a8628dbc68744c324d121a9b0d7cc13297ace
splacentino <39631@heb.be>
2015-11-19 09:45:33 +0100

    refactor directive

--- app/public/scripts/directives/course-list.js
+++ app/public/scripts/directives/course-list.js
@@ -1,10 +1,19 @@
 angular.module('hapi-learning')
-    .directive('courseList', function () {
+    .directive('courseList', ['CoursesFactory', function (CoursesFactory) {
         return {
             restrict: 'E',
             templateUrl: 'scripts/directives/course-list.html',
             link: function(scope, elem, attrs) {
-                scope.courses = scope.$eval(attrs.courses);
+                scope.courses = [];
+                
+                CoursesFactory.load()
+                .then(function(courses) {
+                    if (courses)
+                    {
+                        scope.courses = courses;
+                    }
+                })
+                .catch(function(error) {console.log(error);});
             }
         };
-    });
+    }]);
