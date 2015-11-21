ce7b8b2729300cb632c928998a7547a8d0a2bc8a
Florian Knop <florian_knop@hotmail.com>
2015-11-21 13:09:14 +0100

    Add getList()

--- app/public/scripts/services/files-factory.js
+++ app/public/scripts/services/files-factory.js
@@ -2,20 +2,30 @@
 
 angular.module('hapi-learning')
     .factory('FilesFactory', ['Restangular' , '$q', function(Restangular, $q) {
+
+        var internals = {};
         var exports = {};
 
-        exports.getTree = function(course, path) {
+        internals.get = function(course, path, recursive) {
             return $q(function(resolve, reject) {
                 Restangular
-                .all('courses', course)
-                .customGET(path)
-                .then(function(tree) {
-                    resolve(tree);
-                })
-                .catch(function(err) {
-                    reject(err);
-                });
+                    .all('course', course)
+                    .customGET(path, { recursive: recursive })
+                    .then(function(results) {
+                        resolve(results);
+                    })
+                    .catch(function(err) {
+                        reject(err);
+                    });
             });
+        };
+
+        exports.getList = function(course, path) {
+            return internals.get(course, path, false);
+        };
+
+        exports.getTree = function(course, path) {
+            return internals.get(course, path, true);
         };
 
         return exports;
