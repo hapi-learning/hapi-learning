3039428fdf8296f02ba96e31723f1bcfe88d1015
Florian Knop <florian_knop@hotmail.com>
2015-12-05 11:41:37 +0100

    Remove hard-coded news in news ctrl

--- app/public/scripts/controllers/news-controller.js
+++ app/public/scripts/controllers/news-controller.js
@@ -2,30 +2,4 @@
 
 angular
     .module('hapi-learning')
-    .controller('NewsCtrl', ['$scope', function ($scope) {
-
-        $scope.annonces = [
-            {
-                'course': 'ALG',
-                'subject': 'New file added',
-                'description': 'Projet.pdf has been added. Instructions will follow.',
-                'priority': 'info',
-                'createdOn': Date.now()
-            },
-            {
-                'course': 'SYS',
-                'subject': 'You\'re screwed',
-                'description': 'Your mission, if you accept it, is to fail again',
-                'priority': 'warning',
-                'createdOn': Date.now()
-            },
-            {
-                'course': 'ADMIN',
-                'subject': 'Hapi Learning update',
-                'description': 'Don\'t worry it can\'t be worse than now',
-                'priority': 'danger',
-                'createdOn': Date.now()
-            }
-        ];
-
-}]);
+    .controller('NewsCtrl', ['$scope', function ($scope) { }]);
