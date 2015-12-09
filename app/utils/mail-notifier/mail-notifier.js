b0a4691469a65a93b3d052c0f71f22f8cae42f02
pkriwin <39171@heb.be>
2015-12-10 00:06:35 +0100

    mail templating system implemented; news template is working (files added)

---
+++ app/utils/mail-notifier/mail-notifier.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const sendgrid = require('sendgrid')(process.env.SENDGRID_KEY);
+const P        = require('bluebird');
+const Path     = require('path');
+const Fs       = require('fs');
+const hogan    = require('hogan.js');
+
+const newsTemplate         = Fs.readFileSync(Path.join(__dirname, 'mail-templates/news.hjs'), 'utf-8');
+const compiledNewsTemplate = hogan.compile(newsTemplate); 
+
+exports.register = function(server, options, next) {
+
+    const MailNotifier = {};
+        
+    MailNotifier.notifyNews = function(news, user) {
+        
+        return new P(function(resolve, reject){
+            
+            sendgrid.send({
+                
+                to: user.get('email'),
+                from: process.env.OFFICIAL_EMAIL_ADDRESS,
+                subject: 'News : ' + news.subject,
+                html : compiledNewsTemplate.render(news)
+            }, 
+            function(err) {
+                if (err)
+                    reject(err);
+                else 
+                    resolve();
+            });
+        })  
+    }
+    
+    server.expose('mail-notifier', MailNotifier);
+    
+    next();
+};
+
+exports.register.attributes = {
+    
+    name: 'mail-notifier',
+    version: require('../../../package.json').version,
+};
