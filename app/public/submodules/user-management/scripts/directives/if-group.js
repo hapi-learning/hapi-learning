ad6ca32c7bbf9af6a0cdff63d6dedda409fe046b
Florian Knop <florian_knop@hotmail.com>
2015-12-11 21:58:55 +0100

    Cleanup if-group directive

--- app/public/submodules/user-management/scripts/directives/if-group.js
+++ app/public/submodules/user-management/scripts/directives/if-group.js
@@ -40,30 +40,3 @@ angular.module('hapi-learning.um')
             }
         };
     }]);
-
-  /*  .directive('ifGroup', ['$rootScope',
-                function($rootScope) {
-
-    return {
-        restrict: 'A',
-        link: function(scope, elem, attrs) {
-
-            elem.addClass('ng-hide');
-
-            var groups = attrs['ifGroup'].split(' ');
-            $rootScope.$watch('user', function(user) {
-
-                if (user) {
-                    var role = user.Role.name;
-                    var hasRole = (groups.indexOf(role) !== -1);
-
-                    if (hasRole) {
-                        elem.removeClass('ng-hide');
-                    } else {
-                        elem.addClass('ng-hide');
-                    }
-                }
-            });
-        }
-    };*/
-//}]);
