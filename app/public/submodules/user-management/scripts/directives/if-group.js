ee4da0d6b2f645efb03e20153e59be080cb33758
Florian Knop <florian_knop@hotmail.com>
2015-12-11 21:52:15 +0100

    Fix and close #1

--- app/public/submodules/user-management/scripts/directives/if-group.js
+++ app/public/submodules/user-management/scripts/directives/if-group.js
@@ -2,7 +2,46 @@
 
 
 angular.module('hapi-learning.um')
-    .directive('ifGroup', ['$rootScope',
+    .directive('ifGroup', ['$rootScope', 'ngIfDirective', function($rootScope, ngIfDirective) {
+        var ngIf = ngIfDirective[0];
+
+        return {
+            transclude: ngIf.transclude,
+            priority: ngIf.priority - 1,
+            terminal: ngIf.terminal,
+            restrict: ngIf.restrict,
+            link: function(scope, element, attrs) {
+                var args = arguments;
+                var done = false;
+                $rootScope.$watch('user', function(user) {
+                    if (user && !done) {
+                        var groups = attrs.ifGroup.split(' ');
+                        var role = user.Role.name;
+                        var initialNgIf = attrs.ngIf;
+                        var ifEvaluator;
+
+                        var hasRole = groups.indexOf(role) !== -1;
+
+                        if (initialNgIf) {
+                            ifEvaluator = function() {
+                                return scope.$eval(initialNgIf) && hasRole;
+                            };
+                        } else {
+                            ifEvaluator = function() {
+                                return hasRole;
+                            };
+                        }
+
+                        attrs.ngIf = ifEvaluator;
+                        ngIf.link.apply(ngIf, args);
+                        done = true;
+                    }
+                });
+            }
+        };
+    }]);
+
+  /*  .directive('ifGroup', ['$rootScope',
                 function($rootScope) {
 
     return {
@@ -26,5 +65,5 @@ angular.module('hapi-learning.um')
                 }
             });
         }
-    };
-}]);
+    };*/
+//}]);
