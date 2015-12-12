2d422662cf22ce64503ae5d997db81a1ff99ef19
splacentino <39631@heb.be>
2015-12-12 15:41:12 +0100

    News factory does not store news anymore

--- app/public/scripts/services/news-factory.js
+++ app/public/scripts/services/news-factory.js
@@ -8,84 +8,36 @@ angular.module('hapi-learning')
     '$rootScope',
         function ($q, Restangular, LoginFactory, $rootScope) {
 
-            const internals = {
-                news: [],
-                fetched: false,
-                subscribed: [],
-                subscribedFetched: false,
-                clear: function () {
-                    news = [];
-                    fetched = false;
-                    subscribed = [];
-                    subscribedFetched = false;
-                }
-            };
-
             const exports = {
                 load: function (options) {
-                    console.log(options);
                     return $q(function (resolve) {
-                        if (options.subscribed) 
-                            resolve(internals.subscribedFetched);
-                        else 
-                            resolve(internals.fetched);
-                    }).then(function (fetched) {
-                        if (fetched) {
-                            return options.subscribed ? internals.subscribed : internals.news;
-                        }
-                        else if (options.subscribed) {
-                            console.log('fetch me news');
-                            return Restangular.all('me').get('news').then(function (news) {
-                                internals.subscribed = news;
-                                internals.subscribedFetched = true;
-                                return internals.subscribed;
-                            });
+                        if (options.subscribed) {
+                            return resolve(Restangular.all('me').get('news'));
                         }
                         else {
-                            console.log('fetch every news');
-                            return Restangular.all('news').getList().then(function (news) {
-                                internals.news = news;
-                                internals.fetched = true;
-                                return internals.news;
-                            });
+                            return resolve(Restangular.all('news').getList());
                         }
                     }).then(function (news) {
                         if (options.code) {
-                            console.log('specific news');
-                            return _.filter(news, 'course', options.code);
+                            return _.filter(news, function (news) {
+                                return news.course === options.code;
+                            });
                         }
-                        else
-                        {
-                            console.log('not specific news');
+                        else {
                             return news;
                         }
                     }).then(function (news) {
                         if (typeof options.count === 'number' && options.count > 0 && options.count <= news.length) {
-                            console.log('slice');
                             return news.slice(0, options.count);
                         }
                         else {
-                            console.log('not slice');
                             return news;
                         }
-                    })
-                },
-/*                load: function (count) {
-                    return $q(function (resolve) {
-                        resolve(internals.fetched);
-                    }).then(function (fetched) {
-                        if (fetched) {
-                            return internals.slice(count);
-                        }
-                        else {
-                            return Restangular.all('news').getList().then(function (news) {
-                                internals.news = news;
-                                internals.fetched = true;
-                                return internals.slice(count);
-                            });
-                        }
                     });
-                },*/
+                },
+                loadSpecific: function (code) {
+                    return Restangular.all('courses').one(code).get('news');
+                },
                 add: function (news) {
                     return LoginFactory.getProfile().then(function (profile) {
                         return Restangular.all('news').post({
@@ -96,19 +48,12 @@ angular.module('hapi-learning')
                                 priority: news.priority
                             })
                             .then(function (news) {
-                                if (internals.fetched) {
-                                    internals.news.push(news);
-                                }
                                 $rootScope.$emit('news_added', news);
                                 return news;
                             });
                     });
                 }
-            };
-
-            $rootScope.$on('um.end-session', function () {
-                internals.clear();
-            });
+            };  
 
             return exports;
     }]);
