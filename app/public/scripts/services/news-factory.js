75e1ff4e3086035498e4f766aea7fe641c9ecd81
splacentino <39631@heb.be>
2015-12-12 11:47:10 +0100

    Refactor news-factory to fetch subscribed (or not) news

--- app/public/scripts/services/news-factory.js
+++ app/public/scripts/services/news-factory.js
@@ -11,60 +11,102 @@ angular.module('hapi-learning')
             const internals = {
                 news: [],
                 fetched: false,
-                clear: function() {
-                    fetched = false;
+                subscribed: [],
+                subscribedFetched: false,
+                clear: function () {
                     news = [];
-                },
-                slice: function (count) {
-                    // count has to be a number :
-                    // slice(0, ..) with null return empty, negative numbers return truncated array, ...
-                    if (typeof count === 'number' && count > 0 && count <= internals.news.length) {
-                        return internals.news.slice(0, count);
-                    }
-                    else {
-                        return internals.news;
-                    }
+                    fetched = false;
+                    subscribed = [];
+                    subscribedFetched = false;
                 }
             };
 
-            const exports = {};
-
-            exports.load = function (count) {
-                return $q(function(resolve) {
-                    resolve(internals.fetched);
-                }).then(function(fetched) {
-                    if (fetched) {
-                        return internals.slice(count);
-                    }  else {
-                        return Restangular.all('news').getList().then(function(news) {
-                            internals.news = news;
-                            internals.fetched = true;
-                            return internals.slice(count);
-                        });
-                    }
-                });
-            };
-
-            exports.add = function (news) {
-                return LoginFactory.getProfile().then(function (profile) {
-                    return Restangular.all('news').post({
-                            username: profile.username,
-                            code: news.course ? news.course : null,
-                            content: news.content,
-                            subject: news.subject,
-                            priority: news.priority
-                        })
-                        .then(function (news) {
-                            if (internals.fetched) {
-                                internals.news.push(news);
-                            }
-                            $rootScope.$emit('news_added', news);
+            const exports = {
+                load: function (options) {
+                    console.log(options);
+                    return $q(function (resolve) {
+                        if (options.subscribed) 
+                            resolve(internals.subscribedFetched);
+                        else 
+                            resolve(internals.fetched);
+                    }).then(function (fetched) {
+                        if (fetched) {
+                            return options.subscribed ? internals.subscribed : internals.news;
+                        }
+                        else if (options.subscribed) {
+                            console.log('fetch me news');
+                            return Restangular.all('me').get('news').then(function (news) {
+                                internals.subscribed = news;
+                                internals.subscribedFetched = true;
+                                return internals.subscribed;
+                            });
+                        }
+                        else {
+                            console.log('fetch every news');
+                            return Restangular.all('news').getList().then(function (news) {
+                                internals.news = news;
+                                internals.fetched = true;
+                                return internals.news;
+                            });
+                        }
+                    }).then(function (news) {
+                        if (options.code) {
+                            console.log('specific news');
+                            return _.filter(news, 'course', options.code);
+                        }
+                        else
+                        {
+                            console.log('not specific news');
                             return news;
-                        });
-                });
+                        }
+                    }).then(function (news) {
+                        if (typeof options.count === 'number' && options.count > 0 && options.count <= news.length) {
+                            console.log('slice');
+                            return news.slice(0, options.count);
+                        }
+                        else {
+                            console.log('not slice');
+                            return news;
+                        }
+                    })
+                },
+/*                load: function (count) {
+                    return $q(function (resolve) {
+                        resolve(internals.fetched);
+                    }).then(function (fetched) {
+                        if (fetched) {
+                            return internals.slice(count);
+                        }
+                        else {
+                            return Restangular.all('news').getList().then(function (news) {
+                                internals.news = news;
+                                internals.fetched = true;
+                                return internals.slice(count);
+                            });
+                        }
+                    });
+                },*/
+                add: function (news) {
+                    return LoginFactory.getProfile().then(function (profile) {
+                        return Restangular.all('news').post({
+                                username: profile.username,
+                                code: news.course ? news.course : null,
+                                content: news.content,
+                                subject: news.subject,
+                                priority: news.priority
+                            })
+                            .then(function (news) {
+                                if (internals.fetched) {
+                                    internals.news.push(news);
+                                }
+                                $rootScope.$emit('news_added', news);
+                                return news;
+                            });
+                    });
+                }
             };
 
-            $rootScope.$on('um.end-session', function() {
+            $rootScope.$on('um.end-session', function () {
                 internals.clear();
             });
 
